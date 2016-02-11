a6cd98f71b85e8f24b5815a713ba3e888b8ef1d9
Justin Fargione <justin@lexicalbits.com>
2016-02-10 23:28:42 -0500

    First working version

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+var _ = require('lodash')
+    , Foursquare = require('node-foursquare')
+    , assert = require('assert')
+;
 module.exports = {
     /**
      * The main entry point for the Dexter module
@@ -6,8 +10,42 @@ module.exports = {
      * @param {AppData} dexter Container for all data used in this workflow.
      */
     run: function(step, dexter) {
-        var results = { foo: 'bar' };
-        //Call this.complete with the module's output.  If there's an error, call this.fail(message) instead.
-        this.complete(results);
+        var creds = dexter.provider('foursquare').credentials()
+            , foursquare = Foursquare({ secrets: {
+                clientId: creds.client_id,
+                clientSecret: creds.client_secret,
+                redirectUrl: 'http://foo.bar'
+            } })
+            , lat = step.input('lat').first()
+            , lon = step.input('lon').first()
+            , categories = step.input('categories').toArray()
+            , self = this
+        ;
+        assert(lat);
+        assert(lon);
+        foursquare.Venues.getTrending(lat, lon, {}, null, function(err, data) {
+            if(err) return self.fail(err);
+            self.complete(_.compact(_.map(data.venues, function(venue) {
+                var primaryCategory = null
+                    , isMatch = categories.length === 0
+                ;
+                _.each(venue.categories, function(category) {
+                    if(category.primary) {
+                        primaryCategory = category.name
+                    }
+                    if(categories.length > 0 && categories.indexOf(category.name) === 0) {
+                        isMatch = true;
+                    }
+                });
+
+                return (isMatch) ? {
+                    venue_id: venue.id
+                    , address: venue.location.formattedAddress.join(' ')
+                    , url: venue.url
+                    , name: venue.name
+                    , category: primaryCategory
+                } : null;
+            })));
+        });
     }
 };
